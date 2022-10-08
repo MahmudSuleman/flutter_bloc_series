09d4b2c3b4a51a1049a89b80222686339c87b1d7
Mahmud Chamtooni <38923023+MahmudSuleman@users.noreply.github.com>
2022-10-08 23:11:04 +0000

    counter final

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,9 @@
-import 'package:flutter/material.dart';
+import 'package:bloc/bloc.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_counter/app.dart';
+import 'package:flutter_counter/counter_observer.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
+  Bloc.observer = CounterObserver();
+  runApp(const CounterApp());
 }
